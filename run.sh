1985d6e56ef0bafc911a35285c7fb2ad1068cdb2
liamwibo <60398653+liamwibo@users.noreply.github.com>
2023-12-31 12:58:01 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,17 +5,9 @@ apt upgrade
 
 
 #Install programs.
-sudo apt install docker.io
 sudo apt install curl
 sudo apt install net-tools
-
-
-
-#Install Docker Compose.
-DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
-mkdir -p $DOCKER_CONFIG/cli-plugins
-curl -SL https://github.com/docker/compose/releases/download/v2.23.3/docker-com>
-chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose
+sudo apt install docker-compose
 
 
 
